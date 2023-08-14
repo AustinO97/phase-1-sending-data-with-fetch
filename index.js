88af6e3c1bdc271b94020cb06fdbc268ba031aaf
Austin Oblow <austinoblow97@gmail.com>
2023-08-14 12:25:50 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: name,
+        email: email,
+    }),
+}
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(res => res.json())
+    .then(data => document.querySelector('body').append(data['id']))
+    .catch(function (error) {
+        alert('no');
+        console.log(error.message);
+        document.querySelector('body').append(error.message)
+    })
+};
